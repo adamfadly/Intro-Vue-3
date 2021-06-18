9c3c4d31e5162daaeda21160c0d1797e9548d25b
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-06-18 15:42:16 +0700

    router event detail dynamic

--- src/store/modules/events/event.js
+++ src/store/modules/events/event.js
@@ -1,4 +1,4 @@
-import { fetchEventList } from "./eventService";
+import { fetchEventList, fetchEventDetails } from "./eventService";
 
 // const [SET_EVENT_LIST] = "SET_EVENT_LIST";
 
@@ -7,22 +7,20 @@ export default {
 
   state: {},
 
-  getters: {
-    // getEvents(state) {
-    //   return state.eventsCollection;
-    // },
-  },
+  getters: {},
 
-  mutations: {
-    // SET_EVENT_LIST(state, eventList) {
-    //   state.eventsCollection = eventList;
-    // },
-  },
+  mutations: {},
 
   actions: {
     async getEventList() {
       const eventList = await fetchEventList();
       return eventList;
+    },
+
+    async getEventDetails(id) {
+      const eventDetails = await fetchEventDetails(id);
+      console.log(eventDetails, "vuex");
+      return eventDetails;
     },
   },
 };
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p class="text-bold">{{ event.time }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  props: ["id"],
+  data() {
+    return {
+      event: {},
+    };
+  },
+  methods: {
+    ...mapActions("event", ["getEventDetails"]),
+  },
+
+  async created() {
+    this.event = await this.getEventDetails(this.id);
+    console.log(this.getEventDetails(this.id), "detail");
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
